132f7e780c3bfdc59313d98589da8e0dcbb7a251
Romain BOITARD <rboitard@gmail.com>
2018-10-11 11:25:26 +0200

    adding ffmpeg video generation

--- VideoGenToolSuite/src/VideoGenTestJava1.java
+++ VideoGenToolSuite/src/VideoGenTestJava1.java
@@ -65,6 +65,52 @@ public class VideoGenTestJava1 {
 			e.printStackTrace();
 		}
 	}
+	
+	@Test
+public void testInJava2() throws FileNotFoundException, UnsupportedEncodingException {
+		
+		VideoGeneratorModel videoGen = new VideoGenHelper().loadVideoGenerator(URI.createURI("example1.videogen"));
+		assertNotNull(videoGen);		
+		System.out.println(videoGen.getInformation().getAuthorName());		
+		assertEquals(4, videoGen.getMedias().size());	
+		Random rand = new Random();
+		String playlist = "";
+		EList<Media> medias = videoGen.getMedias();
+		for(Media media: medias) {
+			if(media instanceof Image) {
+				
+			} else if (media instanceof VideoSeq) {
+				VideoSeq vseq = (VideoSeq) media;
+				if (vseq instanceof MandatoryVideoSeq) {
+					String location = ((MandatoryVideoSeq) vseq).getDescription().getLocation();
+					playlist += "file" + " '" + location + "'" + "\n"; 
+				} else if (vseq instanceof OptionalVideoSeq) {
+					int nombreAleatoire = rand.nextInt(2);
+					if(nombreAleatoire == 1) {
+						String location = ((OptionalVideoSeq) vseq).getDescription().getLocation();
+						playlist += "file" + " '" + location + "'" + "\n"; 
+					}
+				} else if (vseq instanceof AlternativeVideoSeq) {
+					EList<VideoDescription> videodesc= ((AlternativeVideoSeq) vseq).getVideodescs();
+					int nombreAleatoire = rand.nextInt(videodesc.size());
+					playlist += "file" + " '" + videodesc.get(nombreAleatoire).getLocation() + "'" + "\n";
+				}
+			}
+		}
+		PrintWriter writer = new PrintWriter("C:/Users/romain/Documents/Travail/IDM/projet/playlist_ffmpeg.txt", "UTF-8");
+		writer.println(playlist);
+		writer.close();
+		String input = "C:\\Users\\romain\\Documents\\Travail\\IDM\\projet\\playlist_ffmpeg.txt";
+		String output = "C:\\Users\\romain\\Documents\\Travail\\IDM\\projet\\output.mp4";
+		String cmd = "ffmpeg -f concat -safe 0 -i " + input + " -c copy " + output;
+		System.out.println(cmd);
+		try {
+			Process p = Runtime.getRuntime().exec(cmd);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
 	
 
